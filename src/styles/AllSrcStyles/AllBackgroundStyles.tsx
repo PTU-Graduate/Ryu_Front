33af81bf38dbee4f25f236adaabf7ace725f1e39
Rchaehyeon <gksmf210601@gmail.com>
2024-04-14 18:42:25 +0900

    feat/

---
+++ src/styles/AllSrcStyles/AllBackgroundStyles.tsx
@@ -0,0 +1,12 @@
+import {StyleSheet} from 'react-native';
+import {deviceHeight, deviceWidth} from '../../utils/DeviceUtils';
+
+export default StyleSheet.create({
+  AllBackground: {
+    flex: 1,
+    backgroundColor: '#ffffff',
+    width: deviceWidth * 1,
+    height: deviceHeight * 1,
+    alignItems: 'center',
+  },
+});
