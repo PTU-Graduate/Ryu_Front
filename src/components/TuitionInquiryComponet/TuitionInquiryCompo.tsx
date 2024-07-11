10f046224e387cc51324dd7e56bb0754dd8ab989
Rchaehyeon <gksmf210601@gmail.com>
2024-07-11 16:48:13 -0400

    feat/설정 및 등록금 납부조회 UI 완성

---
+++ src/components/TuitionInquiryComponet/TuitionInquiryCompo.tsx
@@ -0,0 +1,48 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+import {deviceHeight, deviceWidth} from '../../utils/DeviceUtils';
+import AllTextStyles from '../../styles/AllSrcStyles/AllTextStyles';
+
+interface TuitionInquiryProps {
+  titleText?: string;
+  detailText?: string;
+}
+
+export const TIDetailBar: React.FC<TuitionInquiryProps> = ({
+  titleText,
+  detailText,
+}) => {
+  return (
+    <View
+      style={{
+        justifyContent: 'center',
+        flexDirection: 'row',
+        marginBottom: deviceHeight * 0.01,
+      }}>
+      <View
+        style={{
+          width: deviceWidth * 0.18,
+          height: deviceHeight * 0.045,
+          backgroundColor: '#009B64',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}>
+        <Text style={[AllTextStyles.SemiBold14, {color: '#ffffff'}]}>
+          {titleText}
+        </Text>
+      </View>
+      <View
+        style={{
+          width: deviceWidth * 0.72,
+          borderBottomColor: '#898A8D',
+          borderBottomWidth: deviceWidth * 0.001,
+          justifyContent: 'center',
+        }}>
+        <Text
+          style={[AllTextStyles.SemiBold14, {marginLeft: deviceWidth * 0.04}]}>
+          {detailText}
+        </Text>
+      </View>
+    </View>
+  );
+};
--- src/screens/BottomTabScreens/Setting.tsx
+++ src/screens/BottomTabScreens/Setting.tsx
@@ -1,11 +1,115 @@
-import {View, Text} from 'react-native';
+import {View, Text, TouchableOpacity} from 'react-native';
 import React from 'react';
+import {AllBackground} from '../../components/AllSrcComponets/AllBackground';
+import {AllTitleTopBarCompo} from '../../components/MainSrcCompo/TopBarCompo';
+import {deviceHeight, deviceWidth} from '../../utils/DeviceUtils';
+import AllTextStyles from '../../styles/AllSrcStyles/AllTextStyles';
+import {
+  DetailSettingArrow,
+  DetailSettingButton,
+} from '../../components/SettingComponets/SettingCompo';
 
 const Setting = () => {
   return (
-    <View>
-      <Text>Setting</Text>
-    </View>
+    <AllBackground>
+      <AllTitleTopBarCompo text="설 정" />
+      <View
+        style={{
+          flex: 3,
+          width: deviceWidth * 0.9,
+          marginLeft: deviceWidth * 0.05,
+        }}>
+        <Text
+          style={[AllTextStyles.SemiBold14, {marginLeft: deviceWidth * 0.02}]}>
+          로그인 및 회원정보
+        </Text>
+        <View
+          style={{
+            width: deviceWidth * 0.9,
+            borderBottomColor: '#000000',
+            borderBottomWidth: deviceWidth * 0.004,
+            marginTop: deviceHeight * 0.01,
+          }}></View>
+        <DetailSettingButton text="자동로그인" />
+        <DetailSettingArrow text="비밀번호 변경" />
+        <DetailSettingArrow text="로그아웃" />
+        <DetailSettingButton text="생체인증 설정" />
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            width: deviceWidth * 0.9,
+            height: deviceHeight * 0.06,
+          }}>
+          <Text
+            style={[
+              AllTextStyles.SemiBold13,
+              {marginLeft: deviceWidth * 0.02},
+            ]}>
+            알림 설정
+          </Text>
+          <View
+            style={{
+              width: deviceWidth * 0.1,
+              height: deviceHeight * 0.03,
+              backgroundColor: '#C9C6C6',
+              borderRadius: 10,
+              marginRight: deviceWidth * 0.02,
+            }}>
+            <TouchableOpacity
+              style={{
+                width: deviceWidth * 0.055,
+                height: deviceHeight * 0.03,
+                backgroundColor: '#ffffff',
+                borderColor: '#C9C6C6',
+                borderWidth: 1,
+                borderRadius: 10,
+              }}
+            />
+          </View>
+        </View>
+      </View>
+      <View
+        style={{
+          flex: 4,
+          width: deviceWidth * 0.9,
+          marginLeft: deviceWidth * 0.05,
+        }}>
+        <Text
+          style={[AllTextStyles.SemiBold14, {marginLeft: deviceWidth * 0.02}]}>
+          약관 및 정책
+        </Text>
+        <View
+          style={{
+            width: deviceWidth * 0.9,
+            borderBottomColor: '#000000',
+            borderBottomWidth: deviceWidth * 0.004,
+            marginTop: deviceHeight * 0.01,
+          }}></View>
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            width: deviceWidth * 0.9,
+            height: deviceHeight * 0.06,
+          }}>
+          <Text
+            style={[
+              AllTextStyles.SemiBold13,
+              {marginLeft: deviceWidth * 0.02},
+            ]}>
+            현재 버전
+          </Text>
+          <View style={{marginRight: deviceWidth * 0.02}}>
+            <Text style={[AllTextStyles.SemiBold13, {color: '#BEBEBE'}]}>
+              1.0.3.31
+            </Text>
+          </View>
+        </View>
+      </View>
+    </AllBackground>
   );
 };
 
